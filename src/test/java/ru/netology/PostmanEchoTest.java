48b213d08423aaf5024e73e46fefe7e27e393b22
Lisa Valkevich <lizvalk@mail.ru>
2023-05-27 18:18:17 +0700

    Innitial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void test1() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body()
+        ;
+    }
+}
